7e05a045ded8f981ddb8b091bbf7ff05f3007c1a
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-02-05 06:43:24 +0000

    adding validations

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,7 +5,11 @@ class GramsController < ApplicationController
 
   def create
     @gram = Gram.create(gram_params)
-    redirect_to root_path
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
