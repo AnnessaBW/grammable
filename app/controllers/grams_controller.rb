72e00c3b8d10db204437b1d48a6b705dc56807f4
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-02-05 00:09:33 +0000

    Add a Gram to the Database

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,20 @@
 class GramsController < ApplicationController
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
   def index
   end
+
+private 
+
+def gram_params
+  params.require(:gram).permit(:message)
+end
+
 end
