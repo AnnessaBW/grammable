a641bca271b93fffc8398a411aad457e9942679c
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-02-06 02:33:22 +0000

    modifed factories.rb to add_attribute instead of password

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
     end
-    password "secretPassword"
-    password_confirmation "secretPassword"
+
+    add_attribute(:password) { 'secretPassword' }
+    add_attribute(:password_confirmation) { 'secretPassword' }
   end
 end
